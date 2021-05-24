f809ec6c9977254fd1fe7551dbfa2834fb1f80cb
samirasidd <54817790+samirasidd@users.noreply.github.com>
2021-05-24 13:42:38 +0600

    Add files via upload

---
+++ Section 14/Challenge1_sec14.js
@@ -0,0 +1,24 @@
+
+const Car  = function(make, speed) {
+    this.make = make;
+    this.speed = speed;
+}
+
+Car.prototype.accelerate = function (){
+    console.log(`${this.speed += 10}km/h`);
+}
+
+Car.prototype.brake = function(){
+     console.log(`${(this.speed) +- 5}km/h`);
+}
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+car1.accelerate();
+car1.brake();
+car2.accelerate();
+car2.brake();
+
+
+
