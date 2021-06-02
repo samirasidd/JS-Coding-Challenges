dff0cbcfb1e21d0d0273a82bd6fa67c0de7565c6
samirasidd <54817790+samirasidd@users.noreply.github.com>
2021-06-02 10:24:12 +0600

    Update Challenge1_sec16.js

--- Section 16/Challenge1_sec16.js
+++ Section 16/Challenge1_sec16.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
-
 const renderCountry = function (data, className = '') {
   const html = `
   <article class="country ${className}">
